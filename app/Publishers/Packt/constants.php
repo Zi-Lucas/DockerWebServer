8c35b457662243b76cada34afc518c6da24c8a85
ziLucas <1406835034@qq.com>
2020-04-27 09:36:31 +0800

    web服务搭建

--- app/Publishers/Packt/constants.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Publishers\Packt;
-
-const COUNT = 10;
-const KEY = "217394128rhqf";
-const URL = "www.tiangotech.com";
--- app/memcached.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: hezong
- * Date: 2020-03-14
- * Time: 07:39
- */
